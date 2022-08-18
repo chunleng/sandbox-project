174519c7c0f2cc11b918785e0f2786656e6b8f76
Lim Chun Leng <limchunleng@gmail.com>
2022-08-18 23:24:29 +0900

    counter: try using gloo::storage::localStorage

--- counter/frontend/src/main.rs
+++ counter/frontend/src/main.rs
@@ -1,3 +1,5 @@
+use gloo::storage::LocalStorage;
+use gloo::storage::Storage;
 use yew::prelude::*;
 
 fn main() {
@@ -6,10 +8,28 @@ fn main() {
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let counter = use_state(|| 0);
+    let start_value = LocalStorage::get::<i32>("counter").unwrap_or(0);
+
+    html! {
+        <Counter {start_value} />
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct CounterProps {
+    start_value: i32,
+}
+
+#[function_component(Counter)]
+pub fn counter(props: &CounterProps) -> Html {
+    let counter = use_state(|| props.start_value);
     let onclick = {
         let counter = counter.clone();
-        Callback::from(move |_| counter.set(*counter + 1))
+        Callback::from(move |_| {
+            let new_value = *counter + 1;
+            LocalStorage::set::<i32>("counter", new_value).expect("Failed to set counter");
+            counter.set(new_value);
+        })
     };
 
     html! {
